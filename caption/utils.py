b2ab76353a49f7d553d2f45acfdaf267a4297070
= <phan.ngoclan58@gmail.com>
2017-11-27 13:16:57 +0700

    Fix loader

--- caption/utils.py
+++ caption/utils.py
@@ -17,8 +17,8 @@ def load_image_vgg(path, size=(224, 224)):
     img = img.astype(np.float32, copy=False)
     for c in range(3):
         img[:, :, c] = img[:, :, c] - mean_pixel[c]
-    img = img.transpose((2, 0, 1))
-    img = np.expand_dims(img, axis=0)
+    # img = img.transpose((2, 0, 1))
+    # img = np.expand_dims(img, axis=0)
 
     return img
 
